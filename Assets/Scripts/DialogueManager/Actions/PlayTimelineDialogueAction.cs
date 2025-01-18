4a7b9e4125173eee910c097df35f18490bfd50dd
lolo111channel <49093945+lolo111channel@users.noreply.github.com>
2025-01-18 13:51:42 +0100

    added first quest

---
+++ Assets/Scripts/DialogueManager/Actions/PlayTimelineDialogueAction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CBStuff.DialogueSystem;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace LostInTheSnow
+{
+    [DialogueId("play_timeline")]
+    public class PlayTimelineDialogueAction : IDialogueActionFunc
+    {
+        public void Action(object[] args)
+        {
+            GameObject gameObject = args[0] as GameObject;
+            PlayableDirector playableDirector = gameObject.GetComponent<PlayableDirector>();
+
+
+            playableDirector.Play();
+        }
+    }
+}
